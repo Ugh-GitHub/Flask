5e746702ca69c68dbfb8b1d0163c98eda28756af
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-10-04 14:41:17 -0500

    added get method

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask, json
+import psycopg2
 from markupsafe import escape
+
+# Connect to your postgres DB
+conn = psycopg2.connect("dbname=flask user=peterp")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM companies")
+
+# Retrieve query results
+records = cur.fetchall()
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
@@ -7,7 +20,7 @@ api = Flask(__name__)
 
 @api.route('/companies', methods=['GET'])
 def get_companies():
-  return json.dumps(companies)
+  return json.dumps(records)
 
 @api.route('/companies', methods=['POST'])
 def post_companies():
